f2e87da2a5201b5058cb80bb56d6a842d0cafcb0
Andrew Bancroft <andrewcbancroft@gmail.com>
2014-08-23 17:36:33 -0500

    initial commit

--- SwiftEmailComposer/ViewController.swift
+++ SwiftEmailComposer/ViewController.swift
@@ -1,25 +1,14 @@
-//
-//  ViewController.swift
-//  SwiftEmailComposer
-//
-//  Created by Andrew & Courtney Bancroft on 8/18/14.
-//  Copyright (c) 2014 Andrew Bancroft. All rights reserved.
-//
-
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-                            
+	let emailComposer = EmailComposer()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	@IBAction func sendEmailButtonTapped(sender: AnyObject) {
+		self.emailComposer.present(inViewController: self)
 	}
-
-
 }
-
