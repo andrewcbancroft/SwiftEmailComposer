9508e9a05591c3088812f96f365c871a8e5608e9
Andrew Bancroft <acbancroft@NEWELLMA-MAC.local>
2014-08-28 13:11:29 -0500

    Moved presentation logic from EmailComposer to ViewController in an effort to properly assign responsibilities to each class. Created a wrapper for MFMailComposeViewController.canSendMail() in EmailComposer so that MessageUI does not need to be imported into ViewController to check for email-sending capability.

--- SwiftEmailComposer/EmailComposer.swift
+++ SwiftEmailComposer/EmailComposer.swift
@@ -2,35 +2,23 @@ import Foundation
 import MessageUI
 
 class EmailComposer: NSObject, MFMailComposeViewControllerDelegate {
-	
-	func present(inViewController presentingViewController: UIViewController) {
-		let mailComposeViewController = configuredMailComposeViewController()
-		if MFMailComposeViewController.canSendMail() {
-			presentingViewController.presentViewController(mailComposeViewController, animated: true, completion: nil)
-		} else {
-			self.showSendMailErrorAlert()
-		}
-	}
-	
-	func configuredMailComposeViewController() -> MFMailComposeViewController {
-		let mailComposerVC = MFMailComposeViewController()
-		mailComposerVC.mailComposeDelegate = self
-		
-		mailComposerVC.setToRecipients(["someone@somewhere.com"])
-		mailComposerVC.setSubject("Sending you an in-app e-mail...")
-		mailComposerVC.setMessageBody("Sending e-mail in-app is not so bad!", isHTML: false)
-		
-		return mailComposerVC
-	}
-	
-	func showSendMailErrorAlert() {
-		let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
-		sendMailErrorAlert.show()
-	}
-	
-	// MARK: MFMailComposeViewControllerDelegate Method
-	func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
-		controller.dismissViewControllerAnimated(true, completion: nil)
-	}
-
+    func canSendMail() -> Bool {
+        return MFMailComposeViewController.canSendMail()
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        
+        mailComposerVC.setToRecipients(["someone@somewhere.com"])
+        mailComposerVC.setSubject("Sending you an in-app e-mail...")
+        mailComposerVC.setMessageBody("Sending e-mail in-app is not so bad!", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
--- SwiftEmailComposer/ViewController.swift
+++ SwiftEmailComposer/ViewController.swift
@@ -2,13 +2,19 @@ import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-	let emailComposer = EmailComposer()
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-	}
-	
-	@IBAction func sendEmailButtonTapped(sender: AnyObject) {
-		self.emailComposer.present(inViewController: self)
-	}
+    let emailComposer = EmailComposer()
+    
+    @IBAction func sendEmailButtonTapped(sender: AnyObject) {
+        let configuredMailComposeViewController = emailComposer.configuredMailComposeViewController()
+        if emailComposer.canSendMail() {
+            presentViewController(configuredMailComposeViewController, animated: true, completion: nil)
+        } else {
+            showSendMailErrorAlert()
+        }
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
 }
